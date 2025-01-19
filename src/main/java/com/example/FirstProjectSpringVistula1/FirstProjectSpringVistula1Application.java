70ff460252a50c7456e3fbc48491d166f26a9c59
Krzyzinski <krzyzinskidominik@gmail.com>
2025-01-19 14:31:14 +0100

    Initial commit

---
+++ src/main/java/com/example/FirstProjectSpringVistula1/FirstProjectSpringVistula1Application.java
@@ -0,0 +1,13 @@
+package com.example.FirstProjectSpringVistula1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringVistula1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringVistula1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstProjectSpringVistula1/FirstProjectSpringVistula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstProjectSpringVistula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringVistula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
